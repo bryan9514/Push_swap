a21ce1c0b5c197a7ae6dcbf7ca950498ee13da85
Bryan Turcios <brturcio@1G6.42angouleme.fr>
2025-01-17 18:20:00 +0100

    17-jan-ecole

--- main.c
+++ main.c
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bt24 <bt24@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/15 21:14:48 by brturcio          #+#    #+#             */
-/*   Updated: 2025/01/16 18:28:38 by bt24             ###   ########.fr       */
+/*   Created: 2025/01/17 14:22:01 by brturcio          #+#    #+#             */
+/*   Updated: 2025/01/17 18:19:15 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+
+int	main(int ac, char **av)
+{
+	// t_slack	*slack_a;
+	// t_slack	*slack_b;
+
+	// slack_a = NULL;
+	// slack_b = NULL;
+	if (ac <= 1 ){
+		;
+		return (printf_error(),1);}
+	if (ac >= 2)
+		check_av(av, ac);
+	return (0);
+}
--- push_swap.h
+++ push_swap.h
@@ -5,15 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/15 21:14:32 by brturcio          #+#    #+#             */
-/*   Updated: 2025/01/16 21:40:05 by brturcio         ###   ########.fr       */
+/*   Created: 2025/01/17 14:21:39 by brturcio          #+#    #+#             */
+/*   Updated: 2025/01/17 18:04:06 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef	PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 #include <stdio.h>
-#include <>
+#include <stdlib.h>
+
+typedef struct s_slack
+{
+	int			index;
+	int			value;
+	int			pos;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+} t_slack;
+
+void	check_av(char **av, int ac);
+void	printf_error(void);
+
+
+//split
+static size_t	count_words(char const *str, char c);
+static int	ft_free(char **result, size_t index);
+static void	fill_result(char *new, char const *str, char c);
+static int	div_words(char **result, char const *str, char sep);
+char	**ft_split(char const *s, char c);
 
 #endif
